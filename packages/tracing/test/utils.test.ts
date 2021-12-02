5417dedb5b191beb2de8baaedbc5a505533139d1
Abhijeet Prasad <aprasad@sentry.io>
2021-12-02 14:08:59 -0500

    Update packages/tracing/test/utils.test.ts

--- packages/tracing/test/utils.test.ts
+++ packages/tracing/test/utils.test.ts
@@ -4,11 +4,11 @@ describe('hasTracingEnabled', () => {
   const tracesSampler = () => 1;
   const tracesSampleRate = 1;
   it.each([
-    ['No options', undefined, false],
-    ['No tracesSampler or tracesSampleRate', {}, false],
-    ['With tracesSampler', { tracesSampler }, true],
-    ['With tracesSampleRate', { tracesSampleRate }, true],
-    ['With tracesSampler and tracesSampleRate', { tracesSampler, tracesSampleRate }, true],
+    ['no options', undefined, false],
+    ['no tracesSampler or tracesSampleRate', {}, false],
+    ['with tracesSampler', { tracesSampler }, true],
+    ['with tracesSampleRate', { tracesSampleRate }, true],
+    ['with tracesSampler and tracesSampleRate', { tracesSampler, tracesSampleRate }, true],
   ])(
     '%s',
     (_: string, input: Parameters<typeof hasTracingEnabled>[0], output: ReturnType<typeof hasTracingEnabled>) => {
